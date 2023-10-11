ae05e20421a7fe0139d28169b2e66f087cb7c975
Marina Fernandes <marinafernandess103@gmail.com>
2023-10-11 17:35:21 -0300

    aula 2

--- src/main/java/br/com/marinafe/todolist/user/UserController.java
+++ src/main/java/br/com/marinafe/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.marinafe.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     //recebe dados do usuário
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel); //cria o usuário
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
